f8881a14ae225ecac575e96a7ec19f4ac9447f3b
AlissaStorm <Alissaalidib@hotmail.com>
2024-10-10 18:26:35 +0200

    implemented postgres in inteliliJ

---
+++ src/Refactor/DbFunctions.java
@@ -0,0 +1,16 @@
+package Refactor;
+
+import java.sql.Connection;
+
+public class DbFunctions {
+
+    public Connection connectToDb(){
+        Connectioon connection = null;
+        try{
+            
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
